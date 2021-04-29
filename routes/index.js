022057cdbc23f5249e7163a0da7b9f8644e88037
Nicholai Gallegos <gallegon@oregonstate.edu>
2021-04-29 15:14:54 -0700

    added forever module and updated routing

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/users', function(req, res, next) {
-  res.sendFile('./public/users.html', { root: __dirname});
+  res.sendFile('users.html', { root: './public'});
 });
 router.get('/tickets', function(req, res, next) {
-  res.sendFile('./public/tickets.html', { root: __dirname});
+  res.sendFile('tickets.html', { root: './public'});
 });
 router.get('/browseEvents', function(req, res, next) {
-  res.sendFile('./public/browseEvents.html', { root: __dirname});
+  res.sendFile('browseEvents.html', { root: './public'});
 });
 router.get('/createEvent', function(req, res, next) {
-  res.sendFile('./public/createEvent.html', { root: __dirname});
+  res.sendFile('createEvent.html', { root: './public'});
 });
 router.get('/followPage', function(req, res, next) {
-  res.sendFile('./public/followPage.html', { root: __dirname});
+  res.sendFile('followPage.html', { root: './public'});
 });
 
 module.exports = router;
