c267019cae8761265eb64c430b01d39274cdb94d
Nicholai Gallegos <gallegon@oregonstate.edu>
2021-04-28 23:08:14 -0700

    set up express routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('./public/index.html', { root: __dirname });
+});
+
+router.get('/followPage', function(req, res, next) {
+	res.sendFile('./public/index.html', { root: __dirname});
 });
 
 module.exports = router;
