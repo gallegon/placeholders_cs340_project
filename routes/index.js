2fc3b8f291386f5ba34ca54661473e6579ed2d80
Nicholai Gallegos <gallegon@oregonstate.edu>
2021-04-28 23:58:20 -0700

    Updated routes. Added html for followPage

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,20 @@ router.get('/', function(req, res, next) {
   res.sendFile('./public/index.html', { root: __dirname });
 });
 
+router.get('/users', function(req, res, next) {
+  res.sendFile('./public/users.html', { root: __dirname});
+});
+router.get('/tickets', function(req, res, next) {
+  res.sendFile('./public/tickets.html', { root: __dirname});
+});
+router.get('/browseEvents', function(req, res, next) {
+  res.sendFile('./public/browseEvents.html', { root: __dirname});
+});
+router.get('/createEvent', function(req, res, next) {
+  res.sendFile('./public/createEvent.html', { root: __dirname});
+});
 router.get('/followPage', function(req, res, next) {
-	res.sendFile('./public/index.html', { root: __dirname});
+  res.sendFile('./public/followPage.html', { root: __dirname});
 });
 
 module.exports = router;
